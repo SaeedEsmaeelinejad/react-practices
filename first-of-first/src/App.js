fb28aa3153fd9a9ad4e83842eb71008de2c7fa94
Saeed Esmaeelinejad <s.esmaeelinejad@gmail.com>
2022-07-22 00:30:01 +0430

    use functional component

--- first-of-first/src/App.js
+++ first-of-first/src/App.js
@@ -1,14 +1,20 @@
 import { Component } from "react";
 
-class App extends Component{
-    render(){
-        return (
-        <>
-        <h1>This is reactjs</h1>
-        <h2>it seems this is not reach</h2>
-        </>
-        )
-    }
-}
+// class App extends Component{
+//     render(){
+//         return (
+//         <>
+//         <h1>This is reactjs</h1>
+//         <h2>it seems this is not reach</h2>
+//         </>
+//         )
+//     }
+// }
 
+const App = () => {
+    return ( <>
+    <h1>This is a functional component</h1>
+    </> );
+}
+ 
 export default App;
