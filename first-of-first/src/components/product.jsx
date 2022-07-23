e1c52fff8839e00474a35fe5ff9edae4edf16ee5
Saeed Esmaeelinejad <s.esmaeelinejad@gmail.com>
2022-07-24 01:43:16 +0430

    add product component

---
+++ first-of-first/src/components/product.jsx
@@ -0,0 +1,13 @@
+
+const Product = () => {
+    return ( <>
+    <span className="m-2 text-info">product name </span>
+    <span className="m-2 badge bg-primary">2</span>
+    <button className="m-2 btn btn-sm bg-success">+</button>
+    <button className="m-2 btn btn-sm bg-warning">-</button>
+    <button className="m-2 btn btn-sm bg-danger">Delete</button>
+
+    </> );
+}
+ 
+export default Product;
