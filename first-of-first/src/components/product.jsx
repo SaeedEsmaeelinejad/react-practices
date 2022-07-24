27799474b910201f5afc7526285d6bf04c755cc4
Saeed Esmaeelinejad <s.esmaeelinejad@gmail.com>
2022-07-25 01:21:37 +0430

    using jsx in html

--- first-of-first/src/components/product.jsx
+++ first-of-first/src/components/product.jsx
@@ -1,8 +1,14 @@
 
 const Product = () => {
+    const counter=112;
+    const increase=()=>{
+        counter+=1;    
+        return 0;
+     } 
+
     return ( <>
     <span className="m-2 text-info">product name </span>
-    <span className="m-2 badge bg-primary">2</span>
+    <span className="m-2 badge bg-primary">{counter}</span>
     <button className="m-2 btn btn-sm btn-success">+</button>
     <button className="m-2 btn btn-sm btn-warning">-</button>
     <button className="m-2 btn btn-sm btn-danger">Delete</button>
