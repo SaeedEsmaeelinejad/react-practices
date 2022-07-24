5da9732723b8ff1dc7998d3755019933e232fc95
Saeed Esmaeelinejad <s.esmaeelinejad@gmail.com>
2022-07-25 01:05:51 +0430

    formatting elements

--- first-of-first/src/components/product.jsx
+++ first-of-first/src/components/product.jsx
@@ -3,9 +3,9 @@ const Product = () => {
     return ( <>
     <span className="m-2 text-info">product name </span>
     <span className="m-2 badge bg-primary">2</span>
-    <button className="m-2 btn btn-sm bg-success">+</button>
-    <button className="m-2 btn btn-sm bg-warning">-</button>
-    <button className="m-2 btn btn-sm bg-danger">Delete</button>
+    <button className="m-2 btn btn-sm btn-success">+</button>
+    <button className="m-2 btn btn-sm btn-warning">-</button>
+    <button className="m-2 btn btn-sm btn-danger">Delete</button>
 
     </> );
 }
