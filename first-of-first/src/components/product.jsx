7207c626f23b0f15b61456351d3e0d320b65c5ee
Saeed Esmaeelinejad <s.esmaeelinejad@gmail.com>
2022-07-30 01:05:37 +0430

    use product css

--- first-of-first/src/components/product.jsx
+++ first-of-first/src/components/product.jsx
@@ -1,5 +1,8 @@
+import "./product.css";
+
 const Product = () => {
   const counter = 0;
+  const imageUrl = "https://picsum.photos/300";
   return (
     <>
       <span className="m-2 text-info">product name </span>
@@ -9,6 +12,7 @@ const Product = () => {
       <button className="m-2 btn btn-sm btn-success">+</button>
       <button className="m-2 btn btn-sm btn-warning">-</button>
       <button className="m-2 btn btn-sm btn-danger">Delete</button>
+      <img src={imageUrl} className="product-image" alt="" />
     </>
   );
 };
