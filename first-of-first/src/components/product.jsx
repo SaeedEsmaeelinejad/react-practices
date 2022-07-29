8bf68e26dca6dba0f66885fdf0cbb89ce13a403c
Saeed Esmaeelinejad <s.esmaeelinejad@gmail.com>
2022-07-30 00:54:39 +0430

    using inline if

--- first-of-first/src/components/product.jsx
+++ first-of-first/src/components/product.jsx
@@ -1,17 +1,16 @@
-
 const Product = () => {
-    const counter=112;
-    const list=['saeed', 'simin', 'us'];
-    return ( <>
-    <span className="m-2 text-info">product name </span>
-    <span className="m-2 badge bg-primary">{counter}</span>
-    <button className="m-2 btn btn-sm btn-success">+</button>
-    <button className="m-2 btn btn-sm btn-warning">-</button>
-    <button className="m-2 btn btn-sm btn-danger">Delete</button>
-    <ul>
-        {list.map((item, index)=> <li key={index}>{item}</li>)}
-    </ul>
-    </> );
-}
- 
+  const counter = 0;
+  return (
+    <>
+      <span className="m-2 text-info">product name </span>
+      <span className="m-2 badge bg-primary">
+        {counter == 0 ? "zerp" : counter}
+      </span>
+      <button className="m-2 btn btn-sm btn-success">+</button>
+      <button className="m-2 btn btn-sm btn-warning">-</button>
+      <button className="m-2 btn btn-sm btn-danger">Delete</button>
+    </>
+  );
+};
+
 export default Product;
